3b2e04880051df2d6e6e06b404da8eb0f1b30203
djl <yuuyoo@163.com>
2019-05-28 11:48:41 +0800

    change new info

--- src/com/djl/test/App.java
+++ src/com/djl/test/App.java
@@ -3,7 +3,8 @@ package com.djl.test;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello World");
         // System.out.println("World");
+        int a = 10;
     }
 }
